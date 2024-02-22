c9b7ac965d1018e5491b6d34a612f064e7795f12
Phahose <ekwomnick@gmail.com>
2024-02-21 23:41:46 -0700

    The Tenant Home Page Created

---
+++ CNMaisons/Pages/TenantHome.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CNMaisons.Pages
+{
+    public class TenantHomeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
