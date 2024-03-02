813f793f284e4349d836b5c8eae091b404f143c1
Phahose <ekwomnick@gmail.com>
2024-03-02 01:33:33 -0700

    The add orioerty dine

---
+++ CNMaisons/Pages/AddProperty.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CNMaisons.Pages
+{
+    public class AddPropertyModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
