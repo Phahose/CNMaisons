faeb0404ffdb8e1974f1204afe7c7539584119dc
Phahose <ekwomnick@gmail.com>
2024-03-04 05:39:35 -0700

    The propety details page begun

---
+++ CNMaisons/Pages/PropertyDetails.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CNMaisons.Pages
+{
+    public class PropertyDetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- CNMaisons/wwwroot/js/script.js
+++ CNMaisons/wwwroot/js/script.js
@@ -1,1 +1,22 @@
 ﻿
+const swiper = new Swiper('.swiper', {
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    // Optional parameters
+    loop: true,
+
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true;
+    },
+
+
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+});
