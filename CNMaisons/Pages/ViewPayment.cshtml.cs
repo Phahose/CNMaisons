e046d541b602545a764c223d67a69df96f0d5a13
Ezra Christopher Mallo <62349047+Ezra-Mallo@users.noreply.github.com>
2024-04-16 11:03:10 -0600

    Reapply "These 4 are working now"
    This reverts commit 12d6c9d3fbf85354a53327816900586c2035cc67.

--- CNMaisons/Pages/ViewPayment.cshtml.cs
+++ CNMaisons/Pages/ViewPayment.cshtml.cs
@@ -1,12 +1,15 @@
 using CNMaisons.Controller;
 using CNMaisons.Domain;
 using CNMaisons.TechnicalService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace CNMaisons.Pages
 {
+
+    [Authorize(Roles = "LandLord, Staff, Tenant")]   // Restrict access to specified roles
     public class ViewPaymentModel : PageModel
     {
         public string Message { get; set; } = string.Empty;
@@ -57,7 +60,7 @@ namespace CNMaisons.Pages
             {
                 
                 CNMPMS PaymentRequestDirector = new CNMPMS();
-                PaymentList = PaymentRequestDirector.ViewPaymentbyDate(FindTenantID, FindStartDate, FindEndDate);
+                PaymentList = PaymentRequestDirector.ViewTenantPaymentbyDate(FindTenantID, FindStartDate, FindEndDate);
                 if (PaymentList != null)
                 {
                     Message = "Below are your records.";
