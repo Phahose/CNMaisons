a97cc0dcaf73b488e90a04bea260f843078c7065
Phahose <ekwomnick@gmail.com>
2024-04-22 12:17:34 -0600

    The Forgot Password System begun

--- CNMaisons/Pages/ForgotPassword.cshtml.cs
+++ CNMaisons/Pages/ForgotPassword.cshtml.cs
@@ -1,5 +1,11 @@
+using CNMaisons.Controller;
+using CNMaisons.Domain;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace CNMaisons.Pages
 {
@@ -9,8 +15,63 @@ namespace CNMaisons.Pages
         public string Email { get; set; } = string.Empty;
         public string Message { get; set; } = string.Empty;
         public string AlertClass { get; set; } = string.Empty;
+
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+            CNMPMS controller = new CNMPMS();
+            var user = controller.GetUserByEmail(Email);
+            if (user == null)
+            {
+                Message = "User with provided email does not exist.";
+                AlertClass = "alert-danger";
+                return Page();
+            }
+
+            // Generate a unique token
+            var token = GenerateRandomCode(6);
+
+            // Save token to the database
+            //user.ResetPasswordToken = token;
+           // user.ResetPasswordTokenExpiry = DateTime.UtcNow.AddHours(1); // Token expires in 1 hour
+          //  _context.SaveChanges();
+
+            // Send email to user with password reset link
+            SendPasswordResetEmail(user.Email, token);
+
+            Message = "Password reset link has been sent to your email.";
+            AlertClass = "alert-success";
+            return Page();
+        }
+
+        /* private string GenerateToken()
+         {
+             using (var rng = new RNGCryptoServiceProvider())
+             {
+                 var tokenData = new byte[32];
+                 rng.GetBytes(tokenData);
+                 return Convert.ToBase64String(tokenData);
+             }
+         }*/
+        private string GenerateRandomCode(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+            var random = new Random();
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+        private void SendPasswordResetEmail(string email, string token)
+        {
+            // Code to send email to user with password reset link
+            // You can use any email sending library or service here
+            // Example: SendGrid, MailKit, SmtpClient, etc.
         }
     }
 }
